cff6dc8593a5859a7d37c5dd5dc619d8636728a3
MohdSamiuddin <104500009+MohdSamiuddin@users.noreply.github.com>
2026-08-25 12:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+	const student={
+	name:'Mohd'
+	age:28,
+	city='New Delhi'
+}
+function getKeys(obj){
+	return Object.keys(obj)
+}
+
+console.log(getKeys(student))
